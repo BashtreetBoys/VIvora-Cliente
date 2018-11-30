55f52e9ce3f1610b6d48c2e50726e720f86c955b
Matias De Stefano <matiasdestefano97@gmail.com>
2018-11-30 16:17:21 -0300

    Update MensajeUsuario.java
    Co-Authored-By: Carlos Speranza <speranza5@users.noreply.github.com>

--- src/mensajeria/MensajeUsuario.java
+++ src/mensajeria/MensajeUsuario.java
@@ -5,4 +5,9 @@ public class MensajeUsuario {
 
 	private Usuario usuario;			//Info del usuario
 	private boolean quieroRegistrarme;	//Boolean que indica si el usuario quiere registrarse.
+	
+	public MensajeUsuario(Usuario u, boolean registro) {
+		this.usuario = u;
+		this.quieroRegistrarme = registro;
+	}
 }
