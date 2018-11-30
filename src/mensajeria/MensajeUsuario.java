40289d14b526991a8a9ab915cc1bbfd7757401e4
Matias De Stefano <matiasdestefano97@gmail.com>
2018-11-30 16:13:12 -0300

    MensajeUsuario creado
    Clase para manejar el envio de mensajes entre Servidor y cliente para el Login y el registro.
    
    Co-Authored-By: Carlos Speranza <speranza5@users.noreply.github.com>

---
+++ src/mensajeria/MensajeUsuario.java
@@ -0,0 +1,7 @@
+package mensajeria;
+
+public class MensajeUsuario {
+
+	private Usuario usuario;			//Info del usuario
+	private boolean quieroRegistrarme;	//Boolean que indica si el usuario quiere registrarse.
+}
